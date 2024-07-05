e623f2bd9b3571b7a2755792fa9992f3e723a6db
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-07-05 11:36:48 -0500

    feat: :white_check_mark: 1st and 2nd Consultation
    Correctly finished

--- js/module/movies.js
+++ js/module/movies.js
@@ -17,4 +17,16 @@ export class movies extends connect {
         let res = await this.collection.aggregate().toArray();
         return res
     }
+
+    //1. Buscar películas del género "Accion":
+    async getAccionMovies(){
+        let res = await this.collection.find({genre: "Accion"}).toArray();
+        return res
+    }
+
+    //2. Buscar películas con más de 200 copias en formato "Bluray":
+    async getBluray200(){
+        let res = await this.collection.find({$and: [{"format.name": "Bluray"},{"format.copies": {$gt: 200}}]}, {projection: {}}).toArray();
+        return res
+    }
 }
--- main.js
+++ main.js
@@ -5,4 +5,4 @@ import { movies } from './js/module/movies.js';
 // let obj2 = new connect("Carlos")
 // console.log(obj2)
 let mongo = new movies()
-console.log(console.table(await mongo.getAllMovies()));
+console.log(await mongo.getBluray200());
